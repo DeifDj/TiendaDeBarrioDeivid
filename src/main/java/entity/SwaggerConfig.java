eb9c0075c046d77b2dae7b6ca293174127b12bbf
DeifDj <clikball@gmail.com>
2023-12-06 00:39:51 -0500

    implementacion Swagger

--- src/main/java/entity/SwaggerConfig.java
+++ src/main/java/entity/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.controller")) // Reemplaza con tu paquete de controladores
+                .apis(RequestHandlerSelectors.basePackage(""))
                 .paths(PathSelectors.any())
                 .build();
     }
